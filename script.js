6d064df7851ec59079f027c9fd1cf1c3d0f474d5
Vera <57991898+verchik86@users.noreply.github.com>
2023-01-27 14:51:21 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 window.addEventListener('DOMContentLoaded', ()=>{
+    //tabs
     const tabs = document.querySelectorAll('.tabheader__item'),
            tabsContent = document.querySelectorAll('.tabcontent'),
            tabsParent =  document.querySelector('.tabheader');
@@ -29,5 +30,42 @@ window.addEventListener('DOMContentLoaded', ()=>{
                 }
             });
         }
-    });   
+    });  
+    //timer
+     const timeEnd = '2023-02-01';
+     function getTimeRemaning(endtime) {
+        const t = Date.parse(endtime) - Date.parse(new Date()),
+                days = Math.floor(t / (1000 * 60 * 60 * 24)),
+                hours = Math.floor(t / (1000 * 60 * 24) % 24),
+                minutes = Math.floor((t / 1000 / 60) % 60),
+                seconds = ((t / 1000) % 60);
+        return{
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds,
+        }
+     }
+    function setClock(selector, endtime) {
+        const  timer = document.querySelector(selector),
+               days = document.querySelector('#days'),
+               hours = document.querySelector('#hours'),
+               minutes = document.querySelector('#minutes'),
+               seconds = document.querySelector('#seconds'),
+               timeInterval = setInterval(updateClock, 1000);
+        updateClock ()   
+        function updateClock() {
+            const t = getTimeRemaning(endtime);
+            days.innerHTML = t.days;
+            hours.innerHTML = t.hours;
+            minutes.innerHTML = t.minutes;
+            seconds.innerHTML = t.seconds;
+            if(t.total <= 0){
+              clearInterval(timeInterval); 
+            }
+        }
+
+    } 
+    setClock('timer', timeEnd);
 });
